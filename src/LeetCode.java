a4b898baddd55492a7eb4e9b5b295c7fb0e0041b
wlinktl <wlinkcl@outlook.com>
2023-10-17 13:35:50 -0400

    300 https://leetcode.com/problems/longest-increasing-subsequence/

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -16,6 +16,34 @@ public class LeetCode {
   }
 }
 
+//300: https://leetcode.com/problems/longest-increasing-subsequence/
+class Solution300 {
+  
+  static void solution(){
+      int[] nums= {10,9,2,5,3,7,101,18,20};
+      int len = lengthOfLIS(nums);
+      System.out.println("len=" + len);
+  }
+  
+  static int lengthOfLIS(int[] nums) {
+    // 定义：dp[i] 表示以 nums[i] 这个数结尾的最长递增子序列的长度
+    int[] dp = new int[nums.length];
+    // base case：dp 数组全都初始化为 1
+    Arrays.fill(dp, 1);
+    for (int i = 0; i < nums.length; i++) {
+        for (int j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) 
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+    }
+    
+    int res = 0;
+    for (int i = 0; i < dp.length; i++) {
+        res = Math.max(res, dp[i]);
+    }
+    return res;
+}
+
 // LeetCode 322 Coins Change  https://leetcode.com/problems/coin-change/
 
 //recursive
